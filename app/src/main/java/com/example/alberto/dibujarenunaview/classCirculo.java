48d519ac9a3b0acfb23ed2a24dbfd5a6551edcb4
AlbertoHerediaTorres <albertoherediatorres95@gmail.com>
2017-11-20 18:38:46 +0100

    Completar clase circulo

--- app/src/main/java/com/example/alberto/dibujarenunaview/classCirculo.java
+++ app/src/main/java/com/example/alberto/dibujarenunaview/classCirculo.java
@@ -9,4 +9,10 @@ public class classCirculo {
     int x;
     int y;
     int radio;
+
+    public classCirculo(int x, int y, int radio) {
+        this.x = x;
+        this.y = y;
+        this.radio = radio;
+    }
 }
